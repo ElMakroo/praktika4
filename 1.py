f435a5a44324528ea1d69503e06aeb2d11e88bca
ElMakroo <mister.Villem@gmail.com>
2023-10-15 14:02:49 +0300

    Working code 2.0

--- 1.py
+++ 1.py
@@ -1,6 +1,9 @@
-num = int(input('Введите число: '))
+num = int(input('Введите число, чтобы найти его факториал: '))
+
 fac = 1
+
 while num > 1:
-    fac = fac * num
+    fac *= num
     num = num - 1
+
 print('Факториал числа:',fac)
